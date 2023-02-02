0efaa1d643f390c3258e72c31293309a0716e6ef
Nils <nils.vanhee@pm.me>
2023-02-02 12:09:23 +0100

    make footer component

--- www/pictures.php
+++ www/pictures.php
@@ -156,20 +156,7 @@
   </section>
 
   <!-- footer -->
-  <footer class="text-center py-4 bg-white mt-auto">
-    <a class="btn btn-link btn-floating btn-lg text-dark">
-      <i class="fab fa-facebook-f"> </i>
-    </a>
-    <a class="btn btn-link btn-floating btn-lg text-dark">
-      <i class="fab fa-twitter"> </i>
-    </a>
-    <a class="btn btn-link btn-floating btn-lg text-dark">
-      <i class="fab fa-instagram"> </i>
-    </a>
-    <section class="my-2">
-      © 2022 Copyright | <a class="text-dark serif" href="index.php">The Pet Plate</a>
-    </section>
-  </footer>
+  <?php include("footer.php"); ?>
 </body>
 
 </html>
--- www/restaurants.php
+++ www/restaurants.php
@@ -131,20 +131,7 @@
   </section>
 
   <!-- footer -->
-  <footer class="text-center py-4 bg-white mt-auto">
-    <a class="btn btn-link btn-floating btn-lg text-dark">
-      <i class="fab fa-facebook-f"> </i>
-    </a>
-    <a class="btn btn-link btn-floating btn-lg text-dark">
-      <i class="fab fa-twitter"> </i>
-    </a>
-    <a class="btn btn-link btn-floating btn-lg text-dark">
-      <i class="fab fa-instagram"> </i>
-    </a>
-    <section class="my-2">
-      © 2022 Copyright | <a class="text-dark serif" href="index.php">The Pet Plate</a>
-    </section>
-  </footer>
+  <?php include("footer.php"); ?>
 
   <script
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDDHu_t8VjfZ8JUWdw2QPEV1rQrECGF_os&callback=initMap&v=weekly"
