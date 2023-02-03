060ca2a68fd46934554c49ec6363eefac492c255
Nils <nils.vanhee@pm.me>
2023-02-03 15:44:57 +0100

    add admin page

---
+++ www/admin.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="assets/vendor/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/style.css" />
+  <script src="assets/vendor/bootstrap.min.js"></script>
+  <title>Admin – The Pet Plate</title>
+</head>
+
+<body class="d-flex flex-column vh-100">
+  <?php
+  include("components/navbar.php");
+  ?>
+</body>
+
+</html>
