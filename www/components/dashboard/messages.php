6cab4a100807ebdbdc38754995a85846b5aef8f4
Nils <nils.vanhee@pm.me>
2023-02-08 17:21:16 +0100

    fix no wrap

--- www/components/dashboard/messages.php
+++ www/components/dashboard/messages.php
@@ -15,10 +15,10 @@ $messages = json_decode($data, true);
 ?>
 
 <form method="post">
-  <table class="table table-bordered table-striped table-hover text-nowrap">
+  <table class="table table-striped table-hover">
     <thead>
       <tr>
-        <th>Delete</th>
+        <th></th>
         <th>Date</th>
         <th>Name</th>
         <th>Email</th>
@@ -31,15 +31,15 @@ $messages = json_decode($data, true);
         echo <<<HTML
         <tr>
           <td class="p-0">
-            <button type="submit" name="delete" value="{$message['id']}" class="btn btn-danger w-100">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 24px; height: 24px;">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+            <button type="submit" name="delete" value="{$message['id']}" class="btn btn-danger w-100 rounded-0">
+              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
               </svg>
             </button>
           </td>
-          <td>{$message['created_at']}</td>
-          <td>{$message['first_name']} {$message['last_name']}</td>
-          <td>{$message['email']}</td>
+          <td class="text-nowrap">{$message['created_at']}</td>
+          <td class="text-nowrap">{$message['first_name']} {$message['last_name']}</td>
+          <td class="text-nowrap">{$message['email']}</td>
           <td>{$message['message']}</td>
         </tr>
         HTML;
